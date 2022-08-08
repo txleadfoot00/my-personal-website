95e1cc2f95b862362ea49247b4156c6332e530c0
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-08 15:14:45 -0700

    Update script.js
    Added under construction message

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Under Construction - owner has a full-time day job!";
 document.querySelector("body").appendChild(h2);
